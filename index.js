6a2e5285f7fc3b594ebe7332699150022e113fa0
Radhika S Chhatija <jitbherwani92@gmail.com>
2022-07-22 16:50:03 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const  mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database: 'reimbursement'
+})
+
+con.connect((err)=>{
+    if(err){
+        console.log("Error in Connection");
+    }
+})
+
+app.get('/',(req,res)=>{
+    res.send("select * from conveyance",(err,result)=>{
+        if(err){
+            res.send("Error");
+        }else{
+            res.send(result)
+        }
+    })
+})
+
+app.post('/',(req,res)=>{
+    const data = {
+        from:"september",
+        to:"october",
+        purpose:"Market Visit",
+        mode:"Bike",
+        km:80,
+        InvNo:9,
+        Amt:8000
+    }
+    console.log(req.body)
+    con.query("Insert into conveyance SET ? ",data,(err,result,fields)=>{
+        if(error){
+            return error;
+        }
+        res.send(result)
+    })
+})
